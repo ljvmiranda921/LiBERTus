93946625e08546f03bbeaa7a0273fe9cdb1b382a
Lj Miranda <ljvmiranda@gmail.com>
2023-11-27 19:19:11 +0800

    Add script to evaluate all language pairs

---
+++ benchmarks/evaluate_all_langpairs.sh
@@ -0,0 +1,11 @@
+# Evaluate a model on its own dev and test set (benchmark) and on others too (cross-lingual)
+declare -a arr=("chu" "cop" "fro" "got" "grc" "hbo" "isl" "lat" "latm" "lzh" "ohu" "orv" "san")
+for i in "${arr[@]}"
+do
+    !pip install https://huggingface.co/ljvmiranda921/xx_"$i"_sigtyp_trf/resolve/main/xx_cop_sigtyp_trf-any-py3-none-any.whl
+    for j in "${arr[@]}"
+    do
+        weasel run evaluate-model . --vars.train_lang "$i" --vars.dev_lang "$j" --vars.test_lang "$j"
+    done
+done
+

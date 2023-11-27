6c3f4107c8c75519c773681be95b68cc796b5663
Lj Miranda <ljvmiranda@gmail.com>
2023-11-27 17:56:03 +0800

    Move finetune all in benchmarks folder

--- benchmarks/finetune_all_models.sh
+++ benchmarks/finetune_all_models.sh
@@ -1,5 +1,8 @@
 declare -a arr=("chu" "cop" "fro" "got" "grc" "hbo" "isl" "lat" "latm" "lzh" "ohu" "orv" "san")
+weasel run convert-to-spacy --force
 for i in "${arr[@]}"
 do
     weasel run finetune-model . --vars.train_lang "$i" --vars.dev_lang "$i" 
+    weasel run package-model . --vars.train_lang "$i"
+    weasel push gcs
 done

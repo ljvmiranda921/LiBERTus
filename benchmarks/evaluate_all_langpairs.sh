5b52f92d65c684aacb742ad3ad3e84bc55c99823
Lj Miranda <ljvmiranda@gmail.com>
2023-11-30 09:53:35 +0800

    Update benchmarking scripts

--- benchmarks/evaluate_all_langpairs.sh
+++ benchmarks/evaluate_all_langpairs.sh
@@ -2,7 +2,7 @@
 declare -a arr=("chu" "cop" "fro" "got" "grc" "hbo" "isl" "lat" "latm" "lzh" "ohu" "orv" "san")
 for i in "${arr[@]}"
 do
-    !pip install https://huggingface.co/ljvmiranda921/xx_"$i"_sigtyp_trf/resolve/main/xx_cop_sigtyp_trf-any-py3-none-any.whl
+    !pip install https://huggingface.co/ljvmiranda921/xx_"$i"_sigtyp_trf/resolve/main/xx_"$i"_sigtyp_trf-any-py3-none-any.whl
     for j in "${arr[@]}"
     do
         weasel run evaluate-model . --vars.train_lang "$i" --vars.dev_lang "$j" --vars.test_lang "$j"

73db3c62e832a02b1472cbdfd258c33e077c4bd5
Lj Miranda <ljvmiranda@gmail.com>
2023-12-08 11:44:50 +0800

    [wip] Update

--- paper/plotters/plot_training_curve.py
+++ paper/plotters/plot_training_curve.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import matplotlib.pylab as pylab
 import matplotlib.pyplot as plt
 import typer
+from wasabi import msg
 
 from .constants import MATPLOTLIB_STYLE
 
@@ -12,11 +13,36 @@ pylab.rcParams.update(MATPLOTLIB_STYLE)
 
 def plot_training_curve(input_file: Path, output_file: Path):
     """Plot training curve for the base model"""
+
+    losses = []
     with open(input_file) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         next(csv_reader, None)  # skip the headers
         for row in csv_reader:
-            breakpoint()
+            _, loss, _, _ = row
+            losses.append(float(loss))
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    # Plot actual data
+    ax.plot(losses)
+    ax.set_ylabel("Training Loss")
+    ax.set_xlabel("Steps")
+    ax.set_ylim(bottom=0)
+
+    # Hide the right and top splines
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+
+    fig.tight_layout()
+
+    if output_file.suffix != ".pdf":
+        msg.warn(
+            "File extension is not PDF. I highly recommend"
+            " using that filetype for better resolution."
+        )
+
+    plt.savefig(output_file, transparent=True)
 
 
 if __name__ == "__main__":

64a1455e910ef66c5fc800fb495538e591b8a3a3
Lj Miranda <ljvmiranda@gmail.com>
2023-12-08 11:34:23 +0800

    [wip] Update

--- paper/plotters/plot_training_curve.py
+++ paper/plotters/plot_training_curve.py
@@ -1,2 +1,23 @@
-import typer
+import csv
+from pathlib import Path
+
+import matplotlib.pylab as pylab
 import matplotlib.pyplot as plt
+import typer
+
+from .constants import MATPLOTLIB_STYLE
+
+pylab.rcParams.update(MATPLOTLIB_STYLE)
+
+
+def plot_training_curve(input_file: Path, output_file: Path):
+    """Plot training curve for the base model"""
+    with open(input_file) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=",")
+        next(csv_reader, None)  # skip the headers
+        for row in csv_reader:
+            breakpoint()
+
+
+if __name__ == "__main__":
+    typer.run(plot_training_curve)

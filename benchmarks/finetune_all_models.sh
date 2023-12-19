023f3a50c6b790b6bf99dc03a954fc596e677152
Lj Miranda <ljvmiranda@gmail.com>
2023-12-19 23:07:46 +0800

    [wip] Update

--- benchmarks/finetune_all_models.sh
+++ benchmarks/finetune_all_models.sh
@@ -1,9 +1,11 @@
 # Finetune a multi-task model for each language
-declare -a arr=("chu" "cop" "fro" "got" "grc" "hbo" "isl" "lat" "latm" "lzh" "ohu" "orv" "san")
+declare -a arr=("chu" "cop" "fro" "got" "grc" "hbo" "isl" "lat" "latm" "lzh" "orv" "san")
 weasel run convert-to-spacy --force
 for i in "${arr[@]}"
 do
     weasel run finetune-model . --vars.train_lang "$i" --vars.dev_lang "$i" 
     weasel run package-model . --vars.train_lang "$i"
-    weasel push gcs
 done
+
+spacy train configs/transformer.cfg --output-path training/ohu/ --paths.train corpus/train/ohu_train.spacy --paths.dev corpus/dev/ohu_valid.spacy --system.seed 42 --gpu-id 0 --components.parser.min_action_freq 1 --components.transformer.name ljvmiranda/LiBERTus-base
+weasel run package-model . --vars.train_lang ohu

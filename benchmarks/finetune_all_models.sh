9985de9c22efe637fbe7d15d654115d276469286
Lj Miranda <ljvmiranda@gmail.com>
2023-12-22 09:56:18 +0800

    [wip] Update

--- benchmarks/finetune_all_models.sh
+++ benchmarks/finetune_all_models.sh
@@ -8,10 +8,10 @@ do
 done
 
 # Special cases
-
+# ohu needs to adjust the minimum frequency
 spacy train configs/transformer.cfg --output-path training/ohu/ --paths.train corpus/train/ohu_train.spacy --paths.dev corpus/dev/ohu_valid.spacy --system.seed 42 --gpu-id 0 --components.parser.min_action_freq 1 --components.transformer.name ljvmiranda/LiBERTus-base
 weasel run package-model . --vars.train_lang ohu
-
-
-spacy train configs/transformer.cfg --output-path training/ohu/ --paths.train corpus/train/ohu_train.spacy --paths.dev corpus/dev/ohu_valid.spacy --system.seed 42 --gpu-id 0 --components.parser.min_action_freq 1 --components.transformer.name ljvmiranda/LiBERTus-base
-weasel run package-model . --vars.train_lang ohu
+# chinese needs special tokenizer
+weasel run finetune-trf-model . --vars.train_lang lzh --vars.dev_lang lzh --vars.nlp_lang zh --vars.config transformer_zh.cfg
+weasel run package-model . --vars.train_lang lzh
+# hebrew needs all the help it can get

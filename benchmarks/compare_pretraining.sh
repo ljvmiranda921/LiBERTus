4c8531ffbe660e86e36679d04d54bda184ec769c
Lj Miranda <ljvmiranda@gmail.com>
2023-12-25 16:32:18 +0800

    [wip] Update

--- benchmarks/compare_pretraining.sh
+++ benchmarks/compare_pretraining.sh
@@ -4,5 +4,5 @@ python -m scripts.convert_to_pretrain assets/dev --output-path corpus/control_va
 python -m scripts.convert_to_pretrain assets/train --output-path corpus/up_sampling_train.txt --sampling-strategy upsample --seed 42 --shuffle --verbose --num-choices 1000
 weasel run experiment-sampling . --vars.pretraining_corpus corpus/up_sampling_train.txt --vars.evaluation_corpus corpus/control_variable_dev.txt --vars.name up_sampling --vars.max_steps 20000
 # Averaging
-python -m scripts.convert_to_pretrain assets/train --output-path corpus/avg_sampling_train.txt --sampling-strategy average --seed 42 --shuffle --verbose
+python -m scripts.convert_to_pretrain assets/train --output-path corpus/avg_sampling_train.txt --sampling-strategy average --seed 42 --shuffle --verbose --num-choices 1000
 weasel run experiment-sampling . --vars.pretraining_corpus corpus/avg_sampling_train.txt --vars.evaluation_corpus corpus/control_variable_dev.txt --vars.name avg_sampling --vars.max_steps 20000

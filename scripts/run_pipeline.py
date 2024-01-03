810b7b94b28e7d41b0428ed4244e5485710ce58b
Lj Miranda <ljvmiranda@gmail.com>
2024-01-03 12:34:24 +0800

    [wip] Update

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -42,17 +42,14 @@ def run_pipeline(
             msg.fail("Lang 'orv' has weird parsing errors so must pass a CoNLL-U file")
 
         texts = [sent.metadata["text"] for sent in conllu.parse_incr(input_path.open())]
-        docs = list(nlp.pipe(texts, n_process=n_process))
+        docs = nlp.pipe(texts, n_process=n_process)
     else:
         doc_bin = DocBin().from_disk(input_path)
         _docs = doc_bin.get_docs(nlp.vocab)
-        docs = []
-        for idx, _doc in enumerate(_docs):
-            print(idx, _doc.text)
-            docs.append(nlp(_doc))
-        # docs = list(nlp.pipe(_docs, n_process=n_process))
+        # Convert to text for faster processing
+        texts = [_doc.text for _doc in _docs]
+        docs = nlp.pipe(texts, n_process=n_process)
 
-    msg.info(f"Found {len(docs)} docs in {input_path}.")
     results = {"pos_tagging": [], "morph_features": [], "lemmatisation": []}
     for doc in tqdm(docs):
         sentence = {"pos": [], "morph": [], "lemma": []}

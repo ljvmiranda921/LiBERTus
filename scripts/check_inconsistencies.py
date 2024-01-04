0637e7a8f1dfe0295744926106ab27e792e23ed8
Lj Miranda <ljvmiranda@gmail.com>
2024-01-04 19:24:57 +0800

    [wip] Update

--- scripts/check_inconsistencies.py
+++ scripts/check_inconsistencies.py
@@ -29,14 +29,35 @@ TASK_NAMES = ["lemmatisation", "pos_tagging", "morph_features"]
 def check_inconsistencies(predictions_dir: Path, reference_dir: Path):
     """Check inconsistencies between your predictions and the reference files"""
     for lang in LANGUAGES:
+        sep = "" if lang == "lzh" else " "
+        msg.divider(f"Checking {lang}")
         refs = [
             sentence
             for sentence in conllu.parse_incr(
-                (reference_dir / f"{lang}.conllu").open(encoding="utf-8")
+                (reference_dir / f"{lang}_test.conllu").open(encoding="utf-8")
             )
         ]
         for task in TASK_NAMES:
             preds = srsly.read_json(predictions_dir / task / f"{lang}.json")
+            assert len(refs) == len(preds), "Length of refs and preds not the same!"
+
+            for idx, (ref, pred) in enumerate(zip(refs, preds)):
+                # Check if token lengths are the same
+                ref_text = sep.join([token["form"] for token in ref])
+                if len(ref) != len(pred):
+                    msg.warn(f"Unequal lengths id={idx+1} text={ref_text}")
+                for idx, (tok_ref, tok_pred) in enumerate(zip(ref, pred)):
+                    orth_ref = tok_ref["form"]
+                    if task == "morph_features":
+                        orth_pred = tok_pred["Form"]
+                    elif task == "lemmatisation":
+                        orth_pred, _ = tok_pred
+                    else:  # pos_tagging
+                        orth_pred, _ == tok_pred
+                    if orth_ref != orth_pred:
+                        msg.text(
+                            f"Mismatch tokens id={idx+1}, ref={orth_ref}, pred={orth_pred}"
+                        )
 
 
 if __name__ == "__main__":

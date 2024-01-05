8a3ccc7c7eab76c5afeceb747fc1ed1ebf38ab3f
Lj Miranda <ljvmiranda@gmail.com>
2024-01-05 13:13:38 +0800

    [wip] Update

--- scripts/fill_orv_empty_tokens.py
+++ scripts/fill_orv_empty_tokens.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import List, Optional
+from typing import Any, List, Optional
 
 import conllu
 import srsly
@@ -23,9 +23,17 @@ def fill_orv_empty_tokens(
     refs = [sent for sent in conllu.parse_incr(reference_path.open(encoding="utf-8"))]
     for task in tasks:
         preds = srsly.read_json(submissions_dir / task / "orv.json")
-        for ref, pred in zip(refs, preds):
+        new_preds = []
+        for idx, (ref, pred) in enumerate(zip(refs, preds)):
             ref_tokens = [token["form"] for token in ref]
             pred_tokens = [get_orth(token, task) for token in pred]
+            if not is_equal(ref_tokens, pred_tokens):
+                msg.divider(f"Sentence {idx} not equal!")
+                new_pred = fix_tokens(pred, task=task, ref_tokens=ref_tokens)
+                assert is_equal(
+                    ref_tokens, [get_orth(token, task) for token in new_pred]
+                ), "Still not fixed!"
+                new_preds.append(new_pred)
 
 
 def get_orth(pred, task: str) -> str:
@@ -40,7 +48,23 @@ def get_orth(pred, task: str) -> str:
     return orth
 
 
-def validate(ref_tokens: List[str], pred_tokens: List[str]) -> bool:
+def is_equal(ref_tokens: List[str], pred_tokens: List[str]) -> bool:
+    parity = True
+    if len(ref_tokens) != len(pred_tokens):
+        msg.warn(
+            "Unequal lengths for ref and preds! "
+            f"{len(ref_tokens)} != {len(pred_tokens)}"
+        )
+        parity = False
+
+    for idx, (ref_tok, pred_tok) in enumerate(zip(ref_tokens, pred_tokens)):
+        if ref_tok != pred_tok:
+            msg.warn(f"Position {idx} tokens unequal: {ref_tok} != {pred_tok}")
+
+    return parity
+
+
+def fix_tokens(pred: List[Any], task: str, ref_tokens: List[str]) -> List[Any]:
     pass
 
 

ffa92c98dc2f5f37fefe91aa7db98a8995c907ff
Lj Miranda <ljvmiranda@gmail.com>
2024-01-06 23:40:27 +0800

    [wip] Update

--- submission_p2/load_model.py
+++ submission_p2/load_model.py
@@ -1,15 +1,12 @@
-import spacy
-from spacy.language import Language
-import numpy as np
-from wasabi import msg
 from typing import Any
 
+import numpy as np
+import spacy
+from spacy.language import Language
+from wasabi import msg
 
-def load_model():
-    pass
 
-
-class TrfEmbeddingsDict(dict):
+class TransformerEmbeddings(dict):
     def __init__(self, model_name: str):
         super().__init__()
         self.nlp = spacy.load(model_name)
@@ -18,3 +15,15 @@ class TrfEmbeddingsDict(dict):
     def __getitem__(self, __key: str) -> Any:
         doc = self.nlp(__key)
         return doc.vector.shape
+
+
+def load_model(model_name: str) -> TransformerEmbeddings:
+    if model_name not in spacy.util.get_installed_models():
+        msg.fail(
+            f"Model '{model_name}' not found in your environment. "
+            f" Found: {', '.join(spacy.util.get_installed_models())}",
+            exits=1,
+        )
+
+    embeddings = TransformerEmbeddings(model_name)
+    return embeddings

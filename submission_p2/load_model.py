325dea112241b7d5379300fd21985ddc68bfa88e
Lj Miranda <ljvmiranda@gmail.com>
2024-01-06 23:31:48 +0800

    [wip] Update

--- submission_p2/load_model.py
+++ submission_p2/load_model.py
@@ -1,6 +1,15 @@
-import typer
+import spacy
+from spacy.language import Language
+import numpy as np
 from wasabi import msg
 
 
 def load_model():
     pass
+
+
+class TrfEmbeddingsDict(dict):
+    def __init__(self, model_name: str):
+        super().__init__()
+        self.nlp = spacy.load(model_name)
+        self.nlp.add_pipe("tensor2attr")

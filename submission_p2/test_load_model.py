fa67244f68355f1d2ba855d584e3c2b410a5092c
Lj Miranda <ljvmiranda@gmail.com>
2024-01-07 00:42:02 +0800

    [wip] Update

--- submission_p2/test_load_model.py
+++ submission_p2/test_load_model.py
@@ -1,1 +1,27 @@
+import pytest
+
 from .load_model import load_model
+
+models = [
+    "xx_chu_sigtyp_trf",
+    "el_cop_sigtyp_trf",
+    "xx_fro_sigtyp_trf",
+    "xx_got_sigtyp_trf",
+    "xx_grc_sigtyp_trf",
+    "he_hbo_sigtyp_trf",
+    "xx_isl_sigtyp_trf",
+    "xx_lat_sigtyp_trf",
+    "xx_latm_sigtyp_trf",
+    "zh_lzh_sigtyp_trf",
+    "xx_ohu_sigtyp_trf",
+    "xx_orv_sigtyp_trf",
+    "xx_san_sigtyp_trf",
+]
+
+
+@pytest.mark.parametrize("model", models)
+def test_get_embedding(model):
+    test_word = "hocum"  # should still return a value even if not in a language's vocab
+    trf_model = load_model(model)
+    embedding = trf_model[test_word]
+    assert embedding.shape == (768,)

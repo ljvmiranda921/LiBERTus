13737468309e55bf7b259f2118e2abbe099ee917
Lj Miranda <ljvmiranda@gmail.com>
2024-01-07 00:25:53 +0800

    [wip] Update

--- submission_p2/load_model.py
+++ submission_p2/load_model.py
@@ -62,9 +62,15 @@ def load_model(model_name: str) -> TransformerEmbeddings:
         msg.fail(
             f"Model '{model_name}' not found in your environment. "
             f" Found: {', '.join(spacy.util.get_installed_models())}."
-            "Please install the model from requirements.txt",
+            " Please install the model from requirements.txt",
+            " To get the list of models per language code, import and call "
+            " model_table()",
             exits=1,
         )
 
     embeddings = TransformerEmbeddings(model_name)
     return embeddings
+
+
+def model_table():
+    pass
